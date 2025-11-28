74d71f7792da4fcd355c0f682fb15fba4a4f861f
rahscripts <rahmantjis@gmail.com>
2025-11-29 04:46:49 +0530

    add css to login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -24,33 +24,31 @@ export default function LoginPage() {
         }
     };
     return (
-        <div className="p-10 mx-auto max-w-sm">
+        <div className="min-h-screen flex flex-col items-center justify-center p-10 mx-auto max-w-lg space-y-3">
 
-            <h2 className="text-2xl font-bold mb-4">
-                Login
-            </h2>
-
-            <input
-                className="border p-2 w-full mb-2"
-                type="email"
-                placeholder="abc@gmail.com"
-                onChange={(e) => setEmail(e.target.value)} />
-
-            <input
-                className="border p-2 w-full mb-2"
-                type="password"
-                placeholder="password"
-                onChange={(e) => setPassword(e.target.value)} />
-
-
-            <button
-                onClick={handleLogin}
-                className="bg-blue-500 text-white p-2 rounded cursor-pointer">
-                Login
-            </button>
-            <p>
-                Don't have an account? <a href="/signup" className="text-green-400 underline">Sign up</a>
-            </p>
+            <div className="bg-gray-700 rounded p-8">
+                <h2 className="text-2xl font-bold mb-4">
+                    Login
+                </h2>
+                <input
+                    className="border border-gray-300 rounded hover:border-gray-500 p-2 w-full mb-2"
+                    type="email"
+                    placeholder="abc@gmail.com"
+                    onChange={(e) => setEmail(e.target.value)} />
+                <input
+                    className="border-1 border-gray-300 rounded hover:border-gray-500 p-2 w-full mb-2"
+                    type="password"
+                    placeholder="password"
+                    onChange={(e) => setPassword(e.target.value)} />
+                <button
+                    onClick={handleLogin}
+                    className="bg-green-500 hover:bg-green-600 w-full font-bold text-white p-2 rounded cursor-pointer">
+                    Login
+                </button>
+                <p className="pt-2 ">
+                    Don't have an account? <a href="/signup" className="text-green-400 underline">Sign up</a>
+                </p>
+            </div>
 
         </div>
     )
