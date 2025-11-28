5f21b380c65f4598f3309a2b10b61ac4b8bf6e90
rahscripts <rahmantjis@gmail.com>
2025-11-28 22:55:50 +0530

    starting of personal-notes vault project

---
+++ app/api/auth/signup/route.js
@@ -0,0 +1,25 @@
+import { connectDB } from "../../../../lib/db";
+import User from "../../../../lib/models/User";
+import bcrypt from "bcryptjs";
+
+export async function POST(req) {
+    await connectDB();
+    console.log('signup api hit');
+    const {email, password} = await req.json();
+
+    //check existing user
+    const exists = await User.findOne({ email });
+    if (exists){
+        return Response.json({error: 'User already exists'}, {status: 400})
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        email,
+        password: hashedPassword
+    });
+
+    return Response.json({message: 'signup successfull', userId: user._id}); 
+}
+
